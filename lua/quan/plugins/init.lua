ddce597f3f4833d06970b97bce060b9559ebc901
quan <quannngoc98@gmail.com>
2023-12-13 11:23:28 +0700

    nvim_from_scratch: - setup icon for nvim - setup file init.lua in /plugins for plugins that don't need addition config - setup bufferline (tab bar): just setup, not config yet

---
+++ lua/quan/plugins/init.lua
@@ -0,0 +1,9 @@
+-- this file is used to install plugins that don't need to be configurations
+
+return {
+    "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  
+    -- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  
+    -- "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  }
