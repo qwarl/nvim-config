951fae51b2802e0bc854b180db64073ed1155ffa
quan <quannngoc98@gmail.com>
2023-12-14 14:39:04 +0700

    nvim_from_scratch: - setup gitsigns done

--- lua/quan/plugins/gitsigns.lua
+++ lua/quan/plugins/gitsigns.lua
@@ -1,6 +1,19 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = true,
-  }
-  
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local gitsigns = require("gitsigns")
+		gitsigns.setup({
+			current_line_blame = true,
+			signcolumn = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%m-%d-%Y> - <summary>",
+		})
+	end,
+}
