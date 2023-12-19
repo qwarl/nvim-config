4d0775e3f5ba7b07879728ada259ba21854e4dbd
quan <quannngoc98@gmail.com>
2023-12-19 21:08:58 +0700

    nvim_from_scratch: - update init.lua inside /plugins - setup nvim-surround plugin

--- lua/quan/plugins/init.lua
+++ lua/quan/plugins/init.lua
@@ -3,6 +3,8 @@
 return {
     "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   
+    "nvim-tree/nvim-web-devicons",
+    
     -- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   
     -- "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
