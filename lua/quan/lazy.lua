f4e51fa557896e318853a1479403914fd5564d13
quan <quannngoc98@gmail.com>
2023-12-19 21:06:42 +0700

    nvim_from_scratch: - update lazy.lua - setup which-key plugin

--- lua/quan/lazy.lua
+++ lua/quan/lazy.lua
@@ -1,9 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+        { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+            lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('quan.plugins')
+require('lazy').setup({ { import = 'quan.plugins' }, { import = 'quan.plugins.lsp' } },{
+    checker = {
+        enabled = true,
+        notify = false,
+      },
+      change_detection = {
+        notify = false,
+      },
+})
