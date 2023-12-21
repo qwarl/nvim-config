04e625242d01c0e6eff8423c7e1d250696c259a4
quan <quannngoc98@gmail.com>
2023-12-21 20:17:34 +0700

    nvim_from_scratch: - update options, colorscheme, lazy files

--- lua/quan/lazy.lua
+++ lua/quan/lazy.lua
@@ -6,7 +6,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({ { import = 'quan.plugins' }, { import = 'quan.plugins.lsp' } },{
+require('lazy').setup({ { import = 'quan.plugins' }, { import = 'quan.plugins.lsp' }, {import = 'quan.plugins.notification'} },{
     checker = {
         enabled = true,
         notify = false,
--- lua/quan/plugins/colorscheme.lua
+++ lua/quan/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   'morhetz/gruvbox',
   priority = 1000,
   config = function()
-      vim.cmd([[colorscheme gruvbox]])
+    vim.opt.background = "dark"
+    vim.cmd([[colorscheme gruvbox]])
   end,
 }
