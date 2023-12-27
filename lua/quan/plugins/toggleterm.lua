c717e9844c43539cf7dbadeef4ca16ec4cd6aebc
qwarl <quannngoc98@gmail.com>
2023-12-27 10:23:18 +0700

    nvim_from_scratch: - update toggleterm: change from 'leader t' to 'alt t'

--- lua/quan/plugins/toggleterm.lua
+++ lua/quan/plugins/toggleterm.lua
@@ -1,33 +1,31 @@
 return {
-    'akinsho/toggleterm.nvim',
-    enabled = true,
-    config = function()
-        local toggleterm = require("toggleterm")
-        toggleterm.setup({
-            size = 10,
-            direction = "horizontal",
-            open_mapping = [[<leader>\]],
-            hide_numbers = true,
-            shade_filetypes = {},
-            -- insert_mapping = false,
-            -- start_in_insert = false,
-            persist_size = false,
-        })
+	"akinsho/toggleterm.nvim",
+	enabled = true,
+	config = function()
+		local toggleterm = require("toggleterm")
+		toggleterm.setup({
+			size = 10,
+			direction = "horizontal",
+			open_mapping = [[<A-\>]],
+			hide_numbers = true,
+			shade_filetypes = {},
+			-- insert_mapping = false,
+			-- start_in_insert = false,
+			persist_size = false,
+		})
 
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+			--     vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+			--     vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+			--     vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+			--     vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+			vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+		end
 
-
-        function _G.set_terminal_keymaps()
-            local opts = { buffer = 0 }
-            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        --     vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        --     vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        --     vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        --     vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-        end
-
-        -- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-        vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
-    end
+		-- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+		vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+	end,
 }
