cd7a6d53c5eff3c3a1f341414c0706eeff220c35
qwarl <quannngoc98@gmail.com>
2024-10-24 21:25:59 +0700

    /plugins: paste image from clipboard

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,9 @@
+return {
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{ "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste image from clipboard" },
+	},
+	config = function() end,
+}
