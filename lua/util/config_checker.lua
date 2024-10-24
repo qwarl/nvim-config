75d66c832ae33f7226e71531f9de94903c919c4a
qwarl <quannngoc98@gmail.com>
2024-10-24 17:32:02 +0700

    /util: refactor code

--- lua/util/config_checker.lua
+++ lua/util/config_checker.lua
@@ -3,7 +3,7 @@ local M = {}
 local xdg_config = vim.env.XDG_CONFIG_HOME or vim.env.HOME .. "/.config"
 
 function M.exists_in_config(path)
-  return vim.uv.fs_stat(xdg_config .. "/" .. path) ~= nil
+	return vim.uv.fs_stat(xdg_config .. "/" .. path) ~= nil
 end
 
 return M
