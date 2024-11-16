134899d372ad20eef464e05c76f849c5b07680c5
qwarl <quannngoc98@gmail.com>
2024-11-16 12:16:45 +0700

    mini-lib: refactor code

--- lua/plugins/mini-lib.lua
+++ lua/plugins/mini-lib.lua
@@ -1,48 +1,83 @@
 return {
-	"echasnovski/mini.nvim",
-	version = false,
-	config = function(_, opts)
-		-- setup mini pairs
-		require("mini.pairs").setup()
-
-		-- setup mini icons
-		require("mini.icons").setup()
-
-		-- setup mini surround
-		require("mini.surround").setup({
-			mappings = {
-				add = "gza", -- Add surrounding in Normal and Visual modes
-				delete = "gzd", -- Delete surrounding
-				find = "gzf", -- Find surrounding (to the right)
-				find_left = "gzF", -- Find surrounding (to the left)
-				highlight = "gzh", -- Highlight surrounding
-				replace = "gzr", -- Replace surrounding
-				update_n_lines = "gzn", -- Update `n_lines`
-			},
-		})
-
-		-- setup mini ai
-		require("mini.ai").setup()
-
-		-- setup mini splitjoin
-		require("mini.splitjoin").setup({
-			mappings = {
-				toggle = "sj",
-				split = "",
-				join = "",
-			},
-		})
-
-		-- setup miscellanous utilities
-		require("mini.misc").setup({})
-
-		-- auto root detection for project files
-		require("mini.misc").setup_auto_root({ ".git", "Makefile", "package.json" })
-
-		-- setup mini move
-		require("mini.move").setup({})
-
-		-- setup mini indent
-		require("mini.indentscope").setup()
-	end,
+	-- setup mini pairs
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	-- setup mini icons
+	{
+		"echasnovski/mini.icons",
+		version = false,
+		config = function()
+			require("mini.icons").setup()
+		end,
+	},
+	-- setup mini surround
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = function()
+			require("mini.surround").setup({
+				mappings = {
+					add = "gza", -- Add surrounding in Normal and Visual modes
+					delete = "gzd", -- Delete surrounding
+					find = "gzf", -- Find surrounding (to the right)
+					find_left = "gzF", -- Find surrounding (to the left)
+					highlight = "gzh", -- Highlight surrounding
+					replace = "gzr", -- Replace surrounding
+					update_n_lines = "gzn", -- Update `n_lines`
+				},
+			})
+		end,
+	},
+	-- setup mini ai
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		config = function()
+			require("mini.ai").setup()
+		end,
+	},
+	-- setup mini splitjoin
+	{
+		"echasnovski/mini.splitjoin",
+		version = false,
+		config = function()
+			require("mini.splitjoin").setup({
+				mappings = {
+					toggle = "sj",
+					split = "",
+					join = "",
+				},
+			})
+		end,
+	},
+	-- auto root detection for project files
+	{
+		"echasnovski/mini.misc",
+		version = false,
+		config = function()
+			require("mini.misc").setup({})
+			require("mini.misc").setup_auto_root({ ".git", "Makefile", "package.json" })
+		end,
+	},
+	--setup mini move
+	{
+		"echasnovski/mini.move",
+		version = false,
+		config = function()
+			require("mini.move").setup({})
+		end,
+	},
+	-- setup mini indent
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
 }
