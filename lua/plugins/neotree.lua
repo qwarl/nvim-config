611232f17484d558750934248afa579e80f4f66d
qwarl <quannngoc98@gmail.com>
2025-01-02 11:13:13 +0700

    - neotree: update copy path keybind
    - ufo: initial setup

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -24,6 +24,50 @@ return {
 					},
 				},
 			},
+			commands = {
+				copy_selector = function(state)
+					local node = state.tree:get_node()
+					local filepath = node:get_id()
+					local filename = node.name
+					local modify = vim.fn.fnamemodify
+
+					local vals = {
+						["1. BASENAME"] = modify(filename, ":r"),
+						["2. EXTENSION"] = modify(filename, ":e"),
+						["3. FILENAME"] = filename,
+						["4. PATH (CWD)"] = modify(filepath, ":."),
+						["5. PATH (HOME)"] = modify(filepath, ":~"),
+						["6. PATH"] = filepath,
+						["7. URI"] = vim.uri_from_fname(filepath),
+					}
+
+					local options = vim.tbl_filter(function(val)
+						return vals[val] ~= ""
+					end, vim.tbl_keys(vals))
+					if vim.tbl_isempty(options) then
+						vim.notify("No values to copy", vim.log.levels.WARN)
+						return
+					end
+					table.sort(options)
+					vim.ui.select(options, {
+						prompt = "Choose to copy to clipboard:",
+						format_item = function(item)
+							return ("%s: %s"):format(item, vals[item])
+						end,
+					}, function(choice)
+						local result = vals[choice]
+						if result then
+							vim.notify(("Copied: `%s`"):format(result))
+							vim.fn.setreg("+", result)
+						end
+					end)
+				end,
+			},
+			window = {
+				mappings = {
+					["Y"] = "copy_selector",
+				},
+			},
 		})
 	end,
 	keys = {
